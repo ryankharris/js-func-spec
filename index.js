14ab4f4baa96434f2a447c5fc90fc57fdbbd379b
Ryan K Harris <contact@ryankharris.com>
2017-10-16 14:29:54 -0700

    publish place-holder to npm

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict'
+
+// project: js-func-spec
+// author: Ryan K Harris
+// description: A function specification library that provides run-time type-enforcement rules.
+
+exports.fn = function fn () {
+  return 'Hello. js-func-spec is in development.'
+} // end fn
